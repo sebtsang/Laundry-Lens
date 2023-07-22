d4a36e5a2da39d327ec2d98b17419d677e8d86b2
Sebastian Tsang <52840329+sebtsang@users.noreply.github.com>
2023-07-22 03:39:19 +0000

    Added db folder

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+This is a brief description of your module.
+
+You can provide more details here, including any usage examples or important information.
+"""
+# pylint: disable=E0611
+import cv2
+
+image = cv2.imread('path/to/your/image.jpg')
